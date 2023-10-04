5e7c6d1f2196b0fbe1ff25b18aabd71910aa21c0
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-10-04 20:48:05 +0900

    フォームとボタンを追加

--- app/src/main/java/com/example/jetpacktodoapp/components/EditDialog.kt
+++ app/src/main/java/com/example/jetpacktodoapp/components/EditDialog.kt
@@ -1,15 +1,47 @@
 package com.example.jetpacktodoapp.components
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun EditDialog() {
   AlertDialog(
     onDismissRequest = { /*TODO*/ },
     title = { Text(text = "タスク新規作成") },
-    text = {},
-    buttons = {},
+    text = {
+      Column {
+        Text(text = "タイトル")
+        TextField(value = "", onValueChange = { /* todo */ })
+        Text(text = "詳細")
+        TextField(value = "", onValueChange = { /* todo */ })
+      }
+    },
+    buttons = {
+      Row(
+        modifier = Modifier.padding(8.dp),
+        horizontalArrangement = Arrangement.Center,
+      ) {
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+          modifier = Modifier.width(120.dp),
+          onClick = { /*TODO*/ },
+        ) {
+          Text(text = "キャンセル")
+        }
+        Spacer(modifier = Modifier.width(10.dp))
+        Button(
+          modifier = Modifier.width(120.dp),
+          onClick = { /*TODO*/ },
+        ) {
+          Text(text = "OK")
+        }
+      }
+    },
   )
 }
